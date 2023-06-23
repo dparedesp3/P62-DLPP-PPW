22daf157dd94fdab0b4f9c7a26856be753ff795c
dparedesp3 <dparedesp3@est.ups.edu.ec>
2023-06-23 11:36:31 -0500

    update 1.1

--- .vscode/javascript/DOM/taller_06/script.js
+++ .vscode/javascript/DOM/taller_06/script.js
@@ -1,20 +1,18 @@
 
-
+var image = false;
 
 function upload() {
   if (!image) {
-    var container = document.getElementById('container')
-    var img = document.createElement('img')
-    img.src = 'kirby.png'
-    container.appendChild(img)
-    image = true
+    var container = document.getElementById('container');
+    var img = document.createElement('img');
+    img.src = 'kirby.png';
+    container.appendChild(img);
+    image = true;
   }
 }
 
-var image = false
-
 function delete_img() {
-  var container = document.getElementById('container')
-  container.removeChild(container.lastElementChild)
-  image = false
+  var container = document.getElementById('container');
+  container.removeChild(container.lastElementChild);
+  image = false;
 }
